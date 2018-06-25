866bc3601f1609773094e5fc018f98ca926cbd07
MonsterDuang <31094588+MonsterDuang@users.noreply.github.com>
2018-06-25 17:57:16 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,14 @@ $('.show-small-img').click(function () {
   $('#big-img').attr('src', $(this).attr('src'))
   $(this).attr('alt', 'now').siblings().removeAttr('alt')
   $(this).css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
-  if ($(this).index() >= 3 && $(this).index() < $('#small-img-roll').children().length - 1){
-    $('#small-img-roll').css('left', -($(this).index() - 2) * 76 + 'px')
-  } else if ($(this).index() == $('#small-img-roll').children().length - 1) {
-    $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
-  } else {
-    $('#small-img-roll').css('left', '0')
+  if ($('#small-img-roll').children().length > 4) {
+    if ($(this).index() >= 3 && $(this).index() < $('#small-img-roll').children().length - 1){
+      $('#small-img-roll').css('left', -($(this).index() - 2) * 76 + 'px')
+    } else if ($(this).index() == $('#small-img-roll').children().length - 1) {
+      $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
+    } else {
+      $('#small-img-roll').css('left', '0')
+    }
   }
 })
 // 点击 '>' 下一张
@@ -21,12 +23,14 @@ $('#next-img').click(function (){
   $('#big-img').attr('src', $(".show-small-img[alt='now']").next().attr('src'))
   $(".show-small-img[alt='now']").next().css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
   $(".show-small-img[alt='now']").next().attr('alt', 'now').siblings().removeAttr('alt')
-  if ($(".show-small-img[alt='now']").index() >= 3 && $(".show-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
-    $('#small-img-roll').css('left', -($(".show-small-img[alt='now']").index() - 2) * 76 + 'px')
-  } else if ($(".show-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
-    $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
-  } else {
-    $('#small-img-roll').css('left', '0')
+  if ($('#small-img-roll').children().length > 4) {
+    if ($(".show-small-img[alt='now']").index() >= 3 && $(".show-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
+      $('#small-img-roll').css('left', -($(".show-small-img[alt='now']").index() - 2) * 76 + 'px')
+    } else if ($(".show-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
+      $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
+    } else {
+      $('#small-img-roll').css('left', '0')
+    }
   }
 })
 // 点击 '<' 上一张
@@ -35,11 +39,13 @@ $('#prev-img').click(function (){
   $('#big-img').attr('src', $(".show-small-img[alt='now']").prev().attr('src'))
   $(".show-small-img[alt='now']").prev().css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
   $(".show-small-img[alt='now']").prev().attr('alt', 'now').siblings().removeAttr('alt')
-  if ($(".show-small-img[alt='now']").index() >= 3 && $(".show-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
-    $('#small-img-roll').css('left', -($(".show-small-img[alt='now']").index() - 2) * 76 + 'px')
-  } else if ($(".show-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
-    $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
-  } else {
-    $('#small-img-roll').css('left', '0')
+  if ($('#small-img-roll').children().length > 4) {
+    if ($(".show-small-img[alt='now']").index() >= 3 && $(".show-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
+      $('#small-img-roll').css('left', -($(".show-small-img[alt='now']").index() - 2) * 76 + 'px')
+    } else if ($(".show-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
+      $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
+    } else {
+      $('#small-img-roll').css('left', '0')
+    }
   }
 })
